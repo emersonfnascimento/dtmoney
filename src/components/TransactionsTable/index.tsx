2cb83f1d11faf19bd1284fb3e1ce3c01cfaae712
Emerson Freitas <emersonfreitasn@gmail.com>
2021-04-13 13:36:29 -0500

    listando transações e seeds

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get("transactions").then((response) => console.log(response.data));
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -20,24 +33,16 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de sistema bancário</td>
-            <td className="deposit">R$56.000,00</td>
-            <td>Desenvolvimento</td>
-            <td>02/04/2021</td>
-          </tr>
-          <tr>
-            <td>Feira mensal</td>
-            <td className="withdraw">- R$1.200,00</td>
-            <td>Alimentação</td>
-            <td>01/04/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$1.000,00</td>
-            <td>Moradia</td>
-            <td>15/03/2021</td>
-          </tr>
+          {transactions.map((transaction) => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,30 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de Sistema de contabilidade",
+          type: "deposit",
+          category: "Desenvolvimento",
+          amount: 6000,
+          createdAt: new Date("2021-03-10 11:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1200,
+          createdAt: new Date("2021-03-10 14:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
